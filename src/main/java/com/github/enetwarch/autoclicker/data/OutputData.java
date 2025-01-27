b5cf99e3796ddd8ca5f7fde2c67157dd4c567499
Enetwarch <enetwarch@protonmail.com>
2025-01-27 17:07:39 +0800

    changed delay from 100ms to 10ms

--- src/main/java/com/github/enetwarch/autoclicker/data/OutputData.java
+++ src/main/java/com/github/enetwarch/autoclicker/data/OutputData.java
@@ -5,7 +5,7 @@ import java.awt.event.InputEvent;
 public enum OutputData {
 
     MOUSE_BUTTON(InputEvent.BUTTON1_DOWN_MASK),
-    CLICKER_DELAY(100);
+    CLICKER_DELAY(10);
 
     private final int outputData;
 
